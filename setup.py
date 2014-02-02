2a3ad3d82711075cf487d232f8bdaeae4472e4ca
Jay Deiman <jay@splitstreams.com>
2014-02-02 12:00:51 -0800

    Getting the basics set up

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import sys , glob
+
+try:
+    import boto
+except:
+    print >> sys.stderr , 'You must have python boto installed for this'
+    print >> sys.stderr , '\tsudo apt-get install python-boto'
+    print >> sys.stderr , '\tsudo yum install python-boto'
+    sys.exit(1)
+
+setup(
+    name='r53-dyndns' ,
+    version='0.1.0' ,
+    author='Jay Deiman' ,
+    author_email='admin@splitstreams.com' ,
+    url='https://github.com/crustymonkey/r53-dyndns' ,
+    description='Route53 dynamic DNS agent' ,
+    long_description='Dynamic DNS agent which uses Amazon\'s '
+        'Route53 and your own domain as the dns host.' ,
+    scripts=glob.glob('bin/*') ,
+    data_files=[ ('etc' , glob.glob('etc/*')) ] ,
+    package_dir={'': 'lib'} ,
+    packages=['libr53dyndns'] ,
+    classifiers=[
+        'Development Status :: 4 - Beta' ,
+        'Environment :: Console' ,
+        'Intended Audience :: System Administrators' ,
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)' ,
+        'Natural Language :: English' ,
+        'Topic :: Internet :: Name Service (DNS)' ,
+        'Operating System :: POSIX' ,
+        'Programming Language :: Python' ,
+        'Topic :: System :: Systems Administration' ,
+    ]
+)
