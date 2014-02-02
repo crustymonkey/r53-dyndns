38d8c32841dbeef7a310fbf3e0af916815eb2b09
Jay Deiman <jay@splitstreams.com>
2014-02-02 12:03:14 -0800

    Add the script itself

---
+++ bin/r53-dyndns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+This will run as a dynamic dns agent and update a Route53 resource
+record with your dynamic ip when the IP changes
+"""
+
+from optparse import OptionParser
+
+def getOpts():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
