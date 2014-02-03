89ccb9e026de3ab11529d738c4c5e51314dda4ed
Jay Deiman <jay@splitstreams.com>
2014-02-02 21:57:49 -0800

    Basically finished, but untested

--- bin/r53-dyndns.py
+++ bin/r53-dyndns.py
@@ -6,12 +6,132 @@ record with your dynamic ip when the IP changes
 """
 
 from optparse import OptionParser
+from libr53dyndns.utils import daemonize , writePid , createLogDir , dropPrivs
+from logging.handlers import RotatingFileHandler , StreamHandler
+import libr53dyndns as r53
+import os , logging , time
+
+LOG = None
 
 def getOpts():
-    pass
+    usage = '%prog [options]'
+    p = OptionParser(usage=usage)
+    p.add_option('-c' , '--config' , dest='config' , metavar='FILE' , 
+        default='/etc/r53-dyndns.cfg' , help='Path to the config file '
+        '[default: %default]')
+    p.add_option('-d' , '--daemon' , action='store_true' , default=False ,
+        dest='daemon' , help='Run as a daemon [default: %default]')
+    p.add_option('-p' , '--pidfile' , metavar='FILE' , dest='pidfile' ,
+        default='/var/run/r53-dyndns/r53-dyndns.pid' , help='The pidfile '
+        'for the process.  Note that this will only be created when the '
+        'process is being run as a daemon (-d option) [default: %default]')
+    p.add_option('-D' , '--debug' , action='store_true' , default=False ,
+        dest='debug' , help='Output debugging info [default: %default]')
+    opts , args = p.parse_args()
+    return (opts , args)
+
+def getConfig(opts):
+    conf = r53.DynConfig()
+    conf.read((opts.config,))
+    return conf
+
+def setLogger(opts , conf):
+    global LOG
+    if LOG is None:
+        return
+    logger = logging.getLogger('r53-dyndns')
+    handler = None
+    if opts.daemon:
+        handler = TimedRotatingFileHandler(opts.logfile , 'D' , 
+            backupCount=conf.getint('main' , 'numlogs'))
+    else:
+        handler = StreamHandler()
+    handler.setLevel(logging.INFO)
+    if opts.debug:
+        handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    LOG = logger
+    return logger
+
+def runContinuously(opts , conf):
+    """
+    This will just run in a loop every "update interval"
+    """
+    while True:
+        try:
+            run(opts , conf)
+        except Exception as e:
+            LOG.error('Error trying to check/update IPs: %s' % e)
+        time.sleep(conf.getfloat('main' , 'updateInterval'))
+
+def run(opts , conf):
+    """
+    This will initialize everything and run the check and update any
+    records that need to be updated
+    """
+    ipGet = r53.IPGet(conf.get('main' , 'ipUrl') , 
+        conf.getint('main' , 'iplookuptimeout') ,
+        conf.getint('main' , 'iplookupmaxretries'))
+    curIp = ipGet.getIP()
+    LOG.debug('Current external IP: %s' % curIp)
+    for fqdn in conf.getlist('main' , 'fqdns'):
+        r53Obj = R53(fqdn , conf.get(fqdn , 'zone') ,
+            conf.get(fqdn , 'accesskey') , conf.get(fqdn , 'secretkey') ,
+            cont.getint(fqdn , 'ttl'))
+        r53Ip = r53Obj.getIPR53()
+        LOG.debug('Current IP for %s: %s' % (fqdn , r53Ip))
+        if r53Ip != curIp:
+            LOG.info('Changing IP for %s from %s to %s' % (fqdn , curIp , 
+                r53Ip))
+            r53Obj.update(curIp)
 
 def main():
-    pass
+    opts , args = getOpts()
+    conf = getConfig(opts)
+    if opts.daemon:
+        # Do the things we need to do when we daemonize
+        try:
+            createLogDir(os.path.dirname(conf.get('main' , 'logfile')))
+        except Exception as e:
+            print >> sys.stderr , 'Could not create log directory for ' \
+                'logfile %s: %s' % (conf.get('main' , 'logfile') , e)
+            sys.exit(1)
+        setLogger(opts , conf)
+        daemonize()
+        try:
+            writePid(opts.pidfile , conf.get('main' , 'runasuser') , 
+                conf.get('main' , 'runasgroup'))
+        except Exception as e:
+            LOG.error('Could not write pidfile to %s' % opts.pidfile)
+            sys.exit(1)
+        try:
+            dropPrivs(conf.get('main' , 'runasuser') , 
+                conf.get('main' , 'runasgroup'))
+        except Exception as e:
+            LOG.error('Could not drop privileges to %s/%s: %s' % (
+                conf.get('main' , 'runasuser') , 
+                conf.get('main' , 'runasgroup') , e))
+            sys.exit(1)
+    setLogger(opts , conf)
+    # Set the global log variable
+    if opts.daemon:
+        runContinuously(opts , conf)
+    else:
+        try:
+            run(opts , conf)
+        except Exception as e:
+            LOG.error('Error trying to update IP: %s' % e)
+            sys.exit(1)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        msg = 'Unknown exception occurred: %s' % e
+        if LOG is not None:
+            LOG.error(msg)
+        else:
+            print >> sys.stderr , msg
+        sys.exit(2)
---
+++ lib/libr53dyndns/utils.py
@@ -0,0 +1,57 @@
+
+"""
+Define some utility functions
+"""
+
+import os , sys , pwd , grp
+
+def getUidGid(user , group):
+    """
+    Convenience function to get the corresponding uid/gid for 
+    username/groupname
+    """
+    uid = pwd.getpwnam(user).pw_uid
+    gid = grp.getgrnam(group).gr_gid
+    return (uid , gid)
+
+def dropPrivs(user , group):
+    uid , gid = getUidGid(user , group)
+    os.setregid(gid , gid)
+    os.setreuid(uid , uid)
+
+def createLogDir(logdir , owner , group):
+    if not os.path.isdir(logdir):
+        uid , gid = getUidGid(owner , group)
+        os.makedirs(logdir , 0755)
+        os.chown(logdir , uid , gid)
+
+def daemonize(stdin='/dev/null' , stdout='/dev/null' , stderr='/dev/null')
+    if os.fork() > 0:
+        sys.exit(0)
+    os.chdir('/')
+    os.umask(0)
+    os.setsid()
+    if os.fork() > 0:
+        sys.exit(0)
+
+    for f in (sys.stdout , sys.stderr):
+        f.flush()
+
+    si = open(stdin , 'r')
+    so = open(stdout , 'a+')
+    se = open(stderr , 'a+' , 0)
+    os.dup2(si.fileno() , sys.stdin.fileno())
+    os.dup2(so.fileno() , sys.stdout.fileno())
+    os.dup2(se.fileno() , sys.stderr.fileno())
+
+def writePid(pidFile , owner , group):
+    pidDir = os.path.dirname(pidFile)
+    uid , gid = getUidGid(owner , group)
+    if not os.path.isdir(pidDir):
+        os.makedirs(pidDir , 0755)
+        os.chown(pidDir , uid , gid)
+    pid = os.getpid()
+    fh = open(pidFile , 'w')
+    fh.write(str(pid))
+    fh.close()
+    os.chown(pidFile , uid , gid)
