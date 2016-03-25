34951db23f8b6baf2379b5e3353310f037b85e45
Jay Deiman <jay@splitstreams.com>
2016-03-25 16:53:32 -0700

    Got most of the r53 cutover done, pre-testing

--- libr53dyndns/r53.py
+++ libr53dyndns/r53.py
@@ -1,6 +1,6 @@
 
-from boto.route53.connection import Route53Connection
 import boto3
+import time
 import socket
 
 class R53(object):
@@ -8,7 +8,7 @@ class R53(object):
     Wrap the boto Route53 interface with some specific convenience
     operations
     """
-
+    
     def __init__(self, fqdn , zone , ak , sk , ttl=60):
         """
         Initialize everything given the inputs
@@ -21,28 +21,33 @@ class R53(object):
         ttl:int         The ttl (in seconds) to use for updates.  This 
                         should be something low, like 60 seconds
         """
-        self.bogusIp = '169.254.0.1'
+        self.bogus_v4 = '169.254.0.1'
+        self.bogus_v6 = 'fe80::1'
         self.fqdn = fqdn.lower()
         self.zone = zone.lower()
         self.ttl = int(ttl)
         self._r53 = boto3.client('route53', aws_access_key_id=ak, 
             aws_secret_access_key=sk)
-        self._r53Zone = None
+        self._zone_id = None
 
-    def getIPR53(self, v4=True):
+    def get_ip_r53(self, v4=True):
         """
         Returns the IP currently defined in your Route53 rrset for 
         your fqdn
         """
-        rec = self._getRecord()
+        rec = self._get_record_ip(v4)
         if rec is None:
             # If we don't have a record for this, we will automatically
             # create it with a bogus entry and return
-            self.create(self.bogusIp)
-            return self.bogusIp
-        return rec.to_print()
+            if v4:
+                self.update(ipv4=self.bogus_v4)
+                return self.bogus_v4
+            else:
+                self.update(ipv6=self.bogus_v6)
+                return self.bogus_v6
+        return rec
 
-    def getIPDNS(self, v4=True):
+    def get_ip_dns(self, v4=True):
         """
         This just does a dns lookup to return the IP for the fqdn.  This
         should be faster than hitting the R53 API.  Note that this
@@ -56,26 +61,82 @@ class R53(object):
             elif not v4 and addr[0] == socket.AF_INET6:
                 return addr[4][0]
 
-    def update(self, ip, v4=True):
+    def update(self, ipv4=None, ipv6=None):
         """
-        Update the fqdn with the new IP address
+        Update the fqdn with the new IP addr
         """
-        if self._r53Zone is None:
-            self._r53Zone = self._r53.get_zone(self.zone)
-        self._r53Zone.update_a(self.fqdn, ip , self.ttl)
+        if ivp4 is None and ipv6 is None:
+            raise InvalidInputError('You must specify either (or both) an '
+                'ipv4 address or ipv6 address to update')
+        changes = []
+        if ipv4:
+            chg = self._get_chg_frame()
+            chg['ResourceRecordSet']['ResourceRecords'].append({'Value': ipv4})
+            changes.append(chg)
+        if ipv6:
+            chg = self._get_chg_frame()
+            chg['ResourceRecordSet']['Type'] = 'AAAA'
+            chg['ResourceRecordSet']['ResourceRecords'].append({'Value': ipv4})
+            changes.append(chg)
 
-    def create(self, ip, v4=True):
-        """
-        Create an A record with fqdn and the passed IP address
-        """
-        if self._r53Zone is None:
-            self._r53Zone = self._r53.get_zone(self.zone)
-        self._r53Zone.add_a(self.fqdn, ip , self.ttl)
+        resp = self._r53.change_resource_record_sets(
+            HostedZoneId=self._get_zone_id(),
+            ChangeBatch={
+                'Comment': 'Updated at {0}'.format(time.ctime()),
+                'Changes': changes,
+            },
+        )
 
-    def _getRecord(self):
+        return resp
+
+    def _get_record_ip(self, v4=True):
         """
         Gets the Record object for the fqdn
         """
-        if self._r53Zone is None:
-            self._r53Zone = self._r53.get_zone(self.zone)
-        return self._r53Zone.get_a(self.fqdn)
+        rtype = 'A' if v4 else 'AAAA'
+        resp = self._r53.list_resource_record_sets(
+            HostedZoneId=self._get_zone_id(),
+            StartRecordName=self.fqdn,
+            StartRecordType=rtype,
+            MaxItems=1,
+        )
+        
+        if resp['ResourceRecordSets'][0]['Name'] == self.fqdn and \
+                resp['ResourceRecordSets'][0]['Type'] == rtype:
+            return resp['ResourceRecordSets'][0]['ResourceRecords'][0]['Value']
+
+        return None
+
+    def _get_zone_id(self):
+        """
+        Retrieve the appropriate zone
+        """
+        if self._zone_id is not None:
+            return self._zone_id
+
+        zones = self._53.list_hosted_zones_by_name(self.zone)
+        for zone in zones['HostedZones']:
+            # The first zone should be the one we are looking for, but we
+            # won't make assumptions
+            if zone['Name'] == self.zone:
+                self._zone_id = zone['Id']
+                return self._zone
+
+        # If we get here, the zone isn't found, raise an exception
+        raise ZoneNotFoundError('Could not find the zone: {0}'.format(
+            self.zone))
+    
+    def _get_chg_frame(self):
+        """
+        This gets a baseline setup change batch
+        """
+        chg_framework = {
+            'Action': 'UPSERT',
+            'ResourseRecordSet': {
+                'Name': self.fqdn,
+                'Type': 'A',
+                'ResourceRecords': [],
+            },
+        }
+
+        return chg_framework
